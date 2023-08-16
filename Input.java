55f713bff0a6d58bc999ee075304bc523f2c8a93
Mohit Fating <77158042+MohitFating@users.noreply.github.com>
2023-08-16 23:55:30 +0530

    Create Input.java

---
+++ Input.java
@@ -0,0 +1,14 @@
+package Java;
+import java.util.Scanner;
+
+public class Input {
+
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner (System.in);
+	     System.out.println("Enter number:");
+	     int a = sc.nextInt();
+	     System.out.println("you entered number is : "+a);
+	     sc.close();
+	}
+
+}
