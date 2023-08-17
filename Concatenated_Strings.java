e37d6830ec4f348b631706bb386cf4af4390c967
Mohit Fating <77158042+MohitFating@users.noreply.github.com>
2023-08-17 23:03:36 +0530

    Add files via upload

---
+++ Concatenated_Strings.java
@@ -0,0 +1,12 @@
+package Java;
+
+public class Concatenated_Strings {
+
+	public static void main(String[] args) {
+		Double number =100000000000.0;
+		System.out.println("I am "+"become a invicible");
+		System.out.println("Number is :"+number);
+
+	}
+
+}
---
+++ For_each_Loop.java
@@ -0,0 +1,15 @@
+package Java;
+
+public class For_each_Loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char[] vowels = {'a','e','i','o','u'};
+		
+		for (char item:vowels)
+		{
+			System.out.println(item);
+		}
+	}
+
+}
