1795674c45edb9aaeb993e1724d632f62ccb2725
Mohit Fating <77158042+MohitFating@users.noreply.github.com>
2023-09-05 23:52:25 +0530

    Practise Code
    Data Type Examples

---
+++ Datatype.java
@@ -0,0 +1,51 @@
+package Java;
+
+public class Datatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte b ='L';
+		System.out.println("\n byte = "+b);
+		char ch = 67;
+		System.out.println("\n char = "+ch+"\n");
+		
+		//byte
+		
+		byte MinByte,MaxByte;
+		MinByte=Byte.MIN_VALUE;
+		MaxByte=Byte.MAX_VALUE;
+		
+		System.out.println("Byte Min Value :- "+MinByte);
+		System.out.println("Byte Max Value :- "+MaxByte);
+		
+		byte mybyte = 100;
+		System.out.println("Byte : "+mybyte);
+		
+		short myshort = 500;
+		System.out.println("Short : "+myshort);
+		
+		int myInt = 10000;
+		System.out.println("Int : "+myInt);
+
+		long myLong = 123456789999l;
+		System.out.println("Long : "+myLong);
+
+		float myFloat = 10.000f;
+		System.out.println("Float : "+myFloat);
+
+		double myDouble = 1.2321546;
+		System.out.println("Double : "+myDouble);
+
+		char myChar = 'A';
+		System.out.println("Char : "+myChar);
+
+
+		boolean myBoolean = true;
+		System.out.println("Boolean : "+myBoolean);
+
+		String myString = "Mohit";
+		System.out.println("String : "+myString);
+
+	}
+
+}
